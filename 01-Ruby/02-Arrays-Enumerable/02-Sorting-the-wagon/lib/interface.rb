1779fdd43fd936ef2b1ad271461c5fa5a668a3b0
Lucas Juhel <ljuhel@me.com>
2014-07-09 16:03:15 +0200

    interface exercice finished

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -8,7 +8,16 @@ begin
   name.chomp! if name
 
   # TODO: Add the name we just gotto the students array
+  students << name if name != ""
 
 end while name != ""
-
 # TODO: Call `wagon_sort` method and display the sorted student list
+students = wagon_sort(students)
+
+students_size = students.size
+
+puts "congratulation you have #{students_size} students : "
+
+first_part = students.first( students_size-1).join(", ")
+
+puts " #{first_part} and #{students.last}"
