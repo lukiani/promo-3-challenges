d644273d47da5675c95286d75f1b83a50a22478c
Lucas Juhel <ljuhel@me.com>
2014-07-10 12:09:13 +0200

    i don't succed

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,9 +1,13 @@
 def bank_score
   # TODO: Use Random to get a new random score
-end
 
+  (16..21) to_a.simple
+
+
+  end
 def pick_card
   # TODO: Use Random to get a new random card
+  rand (1..11)
 end
 
 def game_outcome(bank, score)
