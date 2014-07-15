a92f668171d073075ed3caff375422694d63ea97
Lucas Juhel <ljuhel@me.com>
2014-07-15 18:13:07 +0200

    file modified

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
@@ -1,14 +1,10 @@
-def ask_user (param)
-  answer= gets.chomp
-return quets.chomp
-  end
+def compute_name(first_name, middle_name, last_name)
+  # TODO: return full name using string interpolation
+  "#{first_name} #{middle_name} #{last_name}"
+end
 
- puts "what's your first name? "
- first_name= ask_user ("first_name")
- puts" what's your last name ? "
- last_name= ask_user ("first_name")
- name = "#{first_name} #{last_name}"
- Puts "hello class and #{name}"!!!""
+
+
 
   # TODO: return full name using string interpolation
 
