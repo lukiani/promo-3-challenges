26d56848d18f6fbe1251d4ad3d2b251ea4a17d98
Lucas Juhel <ljuhel@me.com>
2014-07-15 18:36:27 +0200

    exercice no solved

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,20 @@
 def poor_calories_counter(burger, side, beverage)
   #TODO: return number of calories for this mcDonald order
+ sum = ["burger", "side", "beverage"]
 end
 
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
+
+
+sum = 0
+sumcalorie = ["Cheese Burger" : 290, "Big Mac" : 300, "Mc Bacon" : 400, "Royal Cheese" : 130,
+"French fries" : 130, "Potatoes" : 130, "Coca" : 160, "Sprite" : 170
+
+order.each ["#{Cheese_burger} + #{big_mac} + #{Mac_bacon} + #{royal_cheese} +
+  #{french_fries} #{Potatoes} + #{Coca} + #{Sprite}"]
 end
+
+puts sumcalorie
+
+
