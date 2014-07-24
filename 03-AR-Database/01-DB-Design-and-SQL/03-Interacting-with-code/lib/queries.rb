5e5eff3fb9fd56123a4bb286ac29f24520fcbe2a
Lucas Juhel <ljuhel@me.com>
2014-07-24 17:40:28 +0200

    exercice solved

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -1,21 +1,28 @@
 require 'sqlite3'
 
 # opens the database
-database_path = File.join(File.dirname(__FILE__), 'db/jukebox.sqlite')
+database_path = File.join(File.dirname(__FILE__),'db/jukebox.sqlite')
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
-  #TODO: count number of rows in table table_name
+#TODO: count number of rows in table table_name
+ db.execute("SELECT COUNT(*) FROM #{table_name}").flatten[0]
 end
+
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
+  db.execute("SELECT name FROM artist ORDER BY name ASC ").flatten
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
+  db.execute("SELECT name FROM track WHERE name lIKE '%love%'").flatten
 end
 
 def long_tracks(db, min_length)
-  #TODO: return tracks verifying: duration > min_length (minutes)
+ #TODO: return tracks verifying: duration > min_length (minutes)
+ min_length = min_length * 60000
+ db.execute("SELECT name FROM track WHERE Milliseconds > #{min_length}").flatten
 end
+
